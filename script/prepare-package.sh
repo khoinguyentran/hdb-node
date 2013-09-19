073157d197b3aa850789bd75440dd814069b7c35
Tran Khoi Nguyen <hans@freebies.i.kaisquare.com>
2013-09-19 12:51:37 +0800

    Refined package generation.

--- script/prepare-package.sh
+++ script/prepare-package.sh
@@ -1,7 +1,13 @@
-PACKAGE_NAME=$1
-PACKAGE_VERSION=$2
-PACKAGE_OS=$3
-PACKAGE_DIR=$4
+REDIST_DIR=$1
+PACKAGE_NAME=$2
+PACKAGE_VERSION=$3
+PACKAGE_OS=$4
+PACKAGE_DIR=$5
+
+if [ -z "$REDIST_DIR" ]; then
+    echo "[EE] Redist directory missing."
+    exit 1
+fi
 
 if [ -z "$PACKAGE_NAME" ]; then
     echo "[EE] Package name missing."
@@ -31,18 +37,20 @@ echo "  Version: $PACKAGE_VERSION"
 echo "  OS: $PACKAGE_OS"
 echo "  Directory: $PACKAGE_DIR"
 echo "----------------------------------------------------------------------"
+PKG_PORT="$REDIST_DIR/ports/$PACKAGE_OS/$PACKAGE_NAME/$PACKAGE_VERSION"
+mkdir -p "$PKG_PORT"
 echo "-- Writing pkg-descr"
-PKG_DESCR="$PACKAGE_DIR/pkg-descr"
+PKG_DESCR="$PKG_PORT/pkg-descr"
 > "$PKG_DESCR"
 echo "Name: $PACKAGE_NAME" >> "$PKG_DESCR"
 echo "Version: $PACKAGE_VERSION" >> "$PKG_DESCR"
 echo "OS: $PACKAGE_OS" >> "$PKG_DESCR"
 
 echo "-- Writing pkg-plist"
-PKG_PLIST="$PACKAGE_DIR/pkg-plist"
+PKG_PLIST="$PKG_PORT/pkg-plist"
 rm -rf "$PKG_PLIST"
 cd "$PACKAGE_DIR"
-FILES=`find -s . -type f`
+FILES=`find . -type f`
 
 # Calculate checkums
 for f in $FILES
